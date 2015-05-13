a0c48e18080848e27567cd6edec04bb95342499d
cedricbrh <cadaric91@hotmail.fr>
2015-05-13 16:35:31 +0200

    creation humain.java

--- src/humain.java
+++ src/humain.java
@@ -1,4 +1,24 @@
 
 public class humain {
+	private String nom;
+	private String boissonFav = "Eau";
+	
+	public humain(String unNom, String unBoissonFav){
+	unNom=nom;
+	unBoissonFav=boissonFav;
+	}
+	
+	public String parle(String texte) {
+		 return  nom+ ' '+texte;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }

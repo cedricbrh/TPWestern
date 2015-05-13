7cd96614d724fc9225011a9d0290c8502d78e260
justineauger <justineauger@aol.com>
2015-05-13 16:33:33 +0200

    creation de la page Histoire, modif de class humain

---
+++ src/Histoire.java
@@ -0,0 +1,10 @@
+
+public class Histoire {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String unHumain = new humain("Henri");
+	}
+
+}
--- src/humain.java
+++ src/humain.java
@@ -1,4 +1,28 @@
 
 public class humain {
-
+	private String nom;
+	private String boisson = "eau";
+	
+	public humain(String unNom){
+		nom = unNom;
+	}
+	
+	public String parle(String texte){
+		return nom + texte;
+	}
+	
+	
+	public String presenter(){
+		return "Bonjour, je suis "+nom+"\nAh un bon verre de "+boisson+" ! GLOUPS !";
+	}
+	
+	public String quelEstTonNom(){
+		return "";
+	}
+	
+	public String quelEstTaBoisson(){
+		return "";
+	}
+	
+	
 }
